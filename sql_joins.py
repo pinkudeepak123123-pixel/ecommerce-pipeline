41bca9be4de6220e5e8c20ab3c8dd4b49591beea
pinkudeepak123123-pixel <pimkudeeoak123123@gmail.com>
2026-03-03 16:50:29 +0530

    Day 6: practiced SQL queries - SUM, AVG, COUNT, MAX with JOINs

--- sql_joins.py
+++ sql_joins.py
@@ -39,3 +39,55 @@ ORDER BY highest_order DESC """)
 rows = cursor.fetchall()
 for row in rows:
     print(row) 
+
+
+cursor.execute("""SELECT l.city ,  SUM(f.amount) as total_revenue 
+               FROM fact_orders f
+               JOIN dim_locations l ON f.location_id = l.location_id
+               GROUP BY l.city
+               ORDER BY total_revenue DESC
+                    """ )
+rows = cursor.fetchall()
+for row in rows:
+    print(row) 
+
+
+## average order amount per product ##
+cursor.execute(""" 
+                   SELECT p.product , AVG(f.amount) as avg_order_amount
+                   FROM fact_orders f 
+                   JOIN dim_products p ON f.product_id = p.product_id
+                   GROUP BY p.product_id 
+                   ORDER BY avg_order_amount DESC""")
+rows = cursor.fetchall()
+for row in rows :
+        print(row)
+
+
+## count of orders per city ##
+
+cursor.execute("""
+                SELECT l.city , COUNT(f.order_id) as total_orders
+                FROM fact_orders f
+                JOIN dim_locations l ON f.location_id = l.location_id 
+                GROUP BY l.city
+                ORDER BY total_orders DESC
+               """)
+rows = cursor.fetchall()
+for row in rows : 
+            print(row)
+             
+## Find the top 3 products by total revenue in Tokyo only ##
+cursor.execute("""
+                SELECT p.product , SUM(f.amount) as total_revenue
+                FROM fact_orders f
+                JOIN dim_products p ON f.product_id = p.product_id
+                JOIN dim_locations l ON f.location_id = l.location_id 
+                WHERE l.city = 'Tokyo'
+                GROUP BY p.product
+                ORDER BY total_revenue DESC 
+                LIMIT 3    
+                """)             
+rows = cursor.fetchall()
+for row in rows:
+       print(row)
